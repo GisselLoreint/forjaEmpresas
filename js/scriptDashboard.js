8115bc0183c311c311a1e9fdd31b5c7ffe16f753
Gissel Diaz <gisseld08@gmail.com>
2022-05-11 12:00:03 -0500

    Se arregla la img de los alert del archivo dashboard.html

--- js/scriptDashboard.js
+++ js/scriptDashboard.js
@@ -3,29 +3,28 @@
 let button2 = document.getElementById("btnSearch");
 button2.addEventListener('click', function () {
     let doc = parseInt(document.getElementById("documento").value);
-
     switch (doc) {
         case 52368795:
             Swal.fire({
-                html: '<img src ="../img/0. logoforja.png">' + '<h3>Cuentas con la 1ra dosis</h3>' + '<i>1ra dosis: </i><br>' + '<b>Farmacéutica:</b> Pfizer<br>' +
+                html: '<img src="../img/0. logoforja.png">' + '<h3>Cuentas con la 1ra dosis</h3>' + '<i>1ra dosis: </i><br>' + '<b>Farmacéutica:</b> Pfizer<br>' +
                     "<b>Fecha:</b> 20/abril/2022",
                 background: '##dfe3ec',
                 color: '#000000'
             })
-            window.setTimeout(() => { window.location.href = "formVacunacion.html" }, 2000);
+            window.setTimeout(() => { window.location.href = "formVacunacion.html" }, 3000);
             break;
 
         case 1001235478:
             Swal.fire({
-                html: '<img src ="../img/0. logoforja.png">' + '<h3>Cuentas con la 1ra y 2da dosis</h3><br>' + '<i>1ra dosis: </i><br>' + '<b>Farmacéutica:</b> Cinovac<br>' + '10/marzo/2022<br><br>' +
+                html: '<img src="../img/0. logoforja.png">' + '<h3>Cuentas con la 1ra y 2da dosis</h3><br>' + '<i>1ra dosis: </i><br>' + '<b>Farmacéutica:</b> Cinovac<br>' + '10/marzo/2022<br><br>' +
                     '<i>2da dosis:</i><br>' + '<b>Farmacéutica:</b> Cinovac<br>' + '08/abril/2022'
             })
-            window.setTimeout(() => { window.location.href = "formVacunacion.html" }, 2000);
+            window.setTimeout(() => { window.location.href = "formVacunacion.html" }, 3000);
             break;
 
         case 25413574:
             Swal.fire({
-                html: '<img src ="../img/0. logoforja.png">' + '<h3>Su esquema de vacunación está completo</h3>' + '<i>1ra dosis:</i><br>' + '<b>Farmacéutica:</b> Moderna<br>' + '28/septiembre/2021<br><br>' +
+                html: '<img src="../img/0. logoforja.png">' + '<h3>Su esquema de vacunación está completo</h3>' + '<i>1ra dosis:</i><br>' + '<b>Farmacéutica:</b> Moderna<br>' + '28/septiembre/2021<br><br>' +
                     '<i>2da dosis:</i><br>' + '<b>Farmacéutica:</b> Moderna<br>' + '21/octubre/2021<br><br>' +
                     '<i>3ra dosis:</i><br>' + '<b>Farmacéutica:</b> Pfizer<br>' + '20/febrero/2022',
                 background: '##dfe3ec',
@@ -35,16 +34,16 @@ button2.addEventListener('click', function () {
 
         case 1007203506:
             Swal.fire({
-                html: '<img src ="../img/0. logoforja.png">' + '<h3>No tiene registro de vacunación</h3>',
+                html: '<img src="../img/0. logoforja.png">>' + '<h3>No tiene registro de vacunación</h3>',
                 background: '##dfe3ec',
                 color: '#000000'
             })
-            window.setTimeout(() => { window.location.href = "formVacunacion.html" }, 2000);
+            window.setTimeout(() => { window.location.href = "formVacunacion.html" }, 3000);
             break;
 
         default:
             Swal.fire({
-                html: '<img src ="../img/0. logoforja.png">' + '<h3>No estas registrado</h3>' + '<h3> ¿Desea realizar el registro?</3>',
+                html: '<img src="../img/0. logoforja.png">' + '<h3>No estas registrado</h3>' + '<h3> ¿Desea realizar el registro?</3>',
                 imageWidth: 400,
                 imageHeight: 200,
                 imageAlt: 'Custom image',
@@ -57,7 +56,7 @@ button2.addEventListener('click', function () {
                 if (result.isConfirmed) {
                     window.setTimeout(() => {
                         window.location.href = "formPacientes.html"
-                    }, 2000);
+                    }, 3000);
                 }
             })
             break;
