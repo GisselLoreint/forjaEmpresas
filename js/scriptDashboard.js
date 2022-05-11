4a2427f2d90903ead50ea841236e3cc01829b1e3
Gissel Diaz <gisseld08@gmail.com>
2022-05-11 12:28:43 -0500

    Se cambio el nombre de la img logoForja y se modifico la ruta de esta img en cada archivo html

--- js/scriptDashboard.js
+++ js/scriptDashboard.js
@@ -6,7 +6,7 @@ button2.addEventListener('click', function () {
     switch (doc) {
         case 52368795:
             Swal.fire({
-                html: '<img src="../img/0. logoforja.png">' + '<h3>Cuentas con la 1ra dosis</h3>' + '<i>1ra dosis: </i><br>' + '<b>Farmacéutica:</b> Pfizer<br>' +
+                html: '<img src="img/logoforja.png">' + '<h3>Cuentas con la 1ra dosis</h3>' + '<i>1ra dosis: </i><br>' + '<b>Farmacéutica:</b> Pfizer<br>' +
                     "<b>Fecha:</b> 20/abril/2022",
                 background: '##dfe3ec',
                 color: '#000000'
@@ -16,7 +16,7 @@ button2.addEventListener('click', function () {
 
         case 1001235478:
             Swal.fire({
-                html: '<img src="../img/0. logoforja.png">' + '<h3>Cuentas con la 1ra y 2da dosis</h3><br>' + '<i>1ra dosis: </i><br>' + '<b>Farmacéutica:</b> Cinovac<br>' + '10/marzo/2022<br><br>' +
+                html: '<img src="img/logoforja.png">' + '<h3>Cuentas con la 1ra y 2da dosis</h3><br>' + '<i>1ra dosis: </i><br>' + '<b>Farmacéutica:</b> Cinovac<br>' + '10/marzo/2022<br><br>' +
                     '<i>2da dosis:</i><br>' + '<b>Farmacéutica:</b> Cinovac<br>' + '08/abril/2022'
             })
             window.setTimeout(() => { window.location.href = "formVacunacion.html" }, 3000);
@@ -24,7 +24,7 @@ button2.addEventListener('click', function () {
 
         case 25413574:
             Swal.fire({
-                html: '<img src="../img/0. logoforja.png">' + '<h3>Su esquema de vacunación está completo</h3>' + '<i>1ra dosis:</i><br>' + '<b>Farmacéutica:</b> Moderna<br>' + '28/septiembre/2021<br><br>' +
+                html: '<img src="img/logoforja.png">' + '<h3>Su esquema de vacunación está completo</h3>' + '<i>1ra dosis:</i><br>' + '<b>Farmacéutica:</b> Moderna<br>' + '28/septiembre/2021<br><br>' +
                     '<i>2da dosis:</i><br>' + '<b>Farmacéutica:</b> Moderna<br>' + '21/octubre/2021<br><br>' +
                     '<i>3ra dosis:</i><br>' + '<b>Farmacéutica:</b> Pfizer<br>' + '20/febrero/2022',
                 background: '##dfe3ec',
@@ -34,7 +34,7 @@ button2.addEventListener('click', function () {
 
         case 1007203506:
             Swal.fire({
-                html: '<img src="../img/0. logoforja.png">>' + '<h3>No tiene registro de vacunación</h3>',
+                html: '<img src="img/logoforja.png">' + '<h3>No tiene registro de vacunación</h3>',
                 background: '##dfe3ec',
                 color: '#000000'
             })
@@ -43,7 +43,7 @@ button2.addEventListener('click', function () {
 
         default:
             Swal.fire({
-                html: '<img src="../img/0. logoforja.png">' + '<h3>No estas registrado</h3>' + '<h3> ¿Desea realizar el registro?</3>',
+                html: '<img src="img/logoforja.png">' + '<h3>No estas registrado</h3>' + '<h3> ¿Desea realizar el registro?</3>',
                 imageWidth: 400,
                 imageHeight: 200,
                 imageAlt: 'Custom image',
